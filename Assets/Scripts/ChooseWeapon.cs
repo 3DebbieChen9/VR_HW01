ab526b8d490e660cee125371c133802c388974a3
Debbie <debbie8839@gmail.com>
2021-03-19 00:57:11 +0800

    Teleport, Choose Weapon done

---
+++ Assets/Scripts/ChooseWeapon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseWeapon : MonoBehaviour
+{
+    public GameObject watergun;
+    public GameObject sword;
+    // Start is called before the first frame update
+    void Start()
+    {
+        watergun.SetActive(true);
+        sword.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void chooseWatergun()
+    {
+        print("Choose Watergun");
+        watergun.SetActive(true);
+        sword.SetActive(false);
+    }
+
+    public void chooseSword()
+    {
+        print("Choose Sword");
+        watergun.SetActive(false);
+        sword.SetActive(true);
+    }
+}
