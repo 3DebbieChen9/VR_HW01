3163f5326b0b8b05828f1548cc909967b6463005
Debbie <debbie8839@gmail.com>
2021-03-19 02:45:51 +0800

    Socket, coin animation done

---
+++ Assets/Scripts/CoinAnimate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinAnimate : MonoBehaviour
+{
+    public GameObject coin;
+    private Animation m_animations;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coin.SetActive(false);
+        m_animations = coin.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playAnimation()
+    {
+        coin.SetActive(true);
+        if (m_animations != null)
+        {
+            m_animations.Play("CoinAnimation");
+        }
+    }
+
+    public void closeCoin()
+    {
+        coin.SetActive(false);
+    }
+}
